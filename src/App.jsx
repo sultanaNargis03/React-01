5dcefed337f12a5609b9ada09d7598d116ae521f
sultanaNargis03 <nargissultana969@gmail.com>
2024-04-02 16:20:56 +0530

    "//conditionally Rendering List Items"

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
   return (
     <div className="App">
       {/* <Hello person={person}/> */}
-      {/* <Fruits /> */}
-      <ConditionalComponent />
+      <Fruits />
+      {/* <ConditionalComponent /> */}
     </div>
   );
 }
--- src/components/Fruit.jsx
+++ src/components/Fruit.jsx
@@ -1,7 +1,14 @@
 export default function Fruit({ name, price, emoji }) {
+  //conditionally Rendering List Items
   return (
-    <li>
-      {emoji} {name} {price}
-    </li>
+    <>
+      {price > 5 ? (
+        <li>
+          {emoji} {name} {price}
+        </li>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
