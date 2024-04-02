cfea694c5c5483056212d8e93021feb7f2006967
sultanaNargis03 <nargissultana969@gmail.com>
2024-04-02 16:34:21 +0530

    "conditionally Rendering a message using ternary operator"

--- src/components/Fruit.jsx
+++ src/components/Fruit.jsx
@@ -1,14 +1,10 @@
-export default function Fruit({ name, price, emoji }) {
-  //conditionally Rendering List Items
+export default function Fruit({ name, price, emoji, soldout }) {
+  //conditionally Rendering a message using ternary operator
   return (
     <>
-      {price > 5 ? (
-        <li>
-          {emoji} {name} {price}
-        </li>
-      ) : (
-        ""
-      )}
+      <li>
+        {emoji} {name} {price} {soldout ? "soldOut" : ""}
+      </li>
     </>
   );
 }
--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -2,11 +2,11 @@ import Fruit from "./Fruit";
 export default function Fruits() {
   // const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"];
   const fruits = [
-    { name: "Apple", price: 10, emoji: "🍎" },
-    { name: "Mango", price: 7, emoji: "🥭" },
-    { name: "Banana", price: 2, emoji: "🍌" },
-    { name: "Orange", price: 5, emoji: "🍊" },
-    { name: "Pineapple", price: 8, emoji: "🍍" },
+    { name: "Apple", price: 10, emoji: "🍎", soldout: false },
+    { name: "Mango", price: 7, emoji: "🥭", soldout: true },
+    { name: "Banana", price: 2, emoji: "🍌", soldout: false },
+    { name: "Orange", price: 5, emoji: "🍊", soldout: false },
+    { name: "Pineapple", price: 8, emoji: "🍍", soldout: true },
   ];
   return (
     <div>
@@ -17,6 +17,7 @@ export default function Fruits() {
             name={fruit.name}
             price={fruit.price}
             emoji={fruit.emoji}
+            soldout={fruit.soldout}
           />
         ))}
       </ul>
