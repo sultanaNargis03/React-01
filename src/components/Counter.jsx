388b81a3e9380750492f53735919cf6fa3a2940f
sultanaNargis03 <nargissultana969@gmail.com>
2024-04-03 15:38:41 +0530

    "state 2"

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,13 +3,17 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
-  function handleClick() {
+  function increment() {
     setCount(count + 1);
+  }
+  function decrement() {
+    setCount(count - 1);
   }
   return (
     <div>
       <h1>Count value is:{count}</h1>
-      <button onClick={handleClick}>Increment</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
